ef6c386143cfa77f3362978b5dcce83325f48f38
Anton <anton@Antons-MacBook-Pro.local>
2020-04-30 22:11:12 -0400

    Results component for listitem display

---
+++ src/components/ResultsDetail.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Image, Text, StyleSheet} from 'react-native';
+
+
+const ResultsDetail = () => {
+    return(
+        <View>
+            <Text>Results Detaul</Text>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default ResultsDetail;
